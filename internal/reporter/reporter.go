362d68be4b4b600311ccbe4ef03ca5c730b7b83a
mcrgnt <microgiant@yandex.ru>
2024-06-30 03:33:49 +0300

    iter9: another fixes for mr

--- internal/reporter/reporter.go
+++ internal/reporter/reporter.go
@@ -28,31 +28,37 @@ type ReportParams struct {
 }
 
 func (t *Reporter) report(params *ReportParams) (string, error) {
-	if buf, err := gzip.Compress(params.Body); err != nil {
+	var (
+		buf       io.Reader
+		err       error
+		req       *http.Request
+		resp      *http.Response
+		bodyBytes []byte
+	)
+	if buf, err = gzip.Compress(params.Body); err != nil {
 		return "", fmt.Errorf("compress failed: %w", err)
-	} else if req, err := http.NewRequest(http.MethodPost, params.URL, buf); err != nil {
-		return "", fmt.Errorf("new request failed: %w", err)
-	} else {
-		req.Header.Set(contentType, applicationJSON)
-		req.Header.Set(contentEncoding, gZip)
-		req.Header.Set(acceptEncoding, gZip)
-
-		if resp, err := http.DefaultClient.Do(req); err != nil {
-			return "", fmt.Errorf("report response failed: %w", err)
-		} else {
-			defer func() {
-				if err := resp.Body.Close(); err != nil {
-					params.Logger.Error().Msg(err.Error())
-				}
-			}()
-
-			if bodyBytes, err := io.ReadAll(resp.Body); err != nil {
-				return "", fmt.Errorf("report response failed: %w", err)
-			} else {
-				return string(bodyBytes), nil
-			}
-		}
 	}
+	if req, err = http.NewRequest(http.MethodPost, params.URL, buf); err != nil {
+		return "", fmt.Errorf("new request failed: %w", err)
+	}
+
+	req.Header.Set(contentType, applicationJSON)
+	req.Header.Set(contentEncoding, gZip)
+	req.Header.Set(acceptEncoding, gZip)
+
+	if resp, err = http.DefaultClient.Do(req); err != nil {
+		return "", fmt.Errorf("report response failed: %w", err)
+	}
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			params.Logger.Error().Msg(err.Error())
+		}
+	}()
+
+	if bodyBytes, err = io.ReadAll(resp.Body); err != nil {
+		return "", fmt.Errorf("report response failed: %w", err)
+	}
+	return string(bodyBytes), nil
 }
 
 func Report(params *ReportParams) (err error) {
