f590bc4908549e88cafd92bce5d3f874463e4db9
mcrgnt <microgiant@yandex.ru>
2024-07-02 17:34:48 +0300

    iter10: just ping

--- internal/db/db.go
+++ internal/db/db.go
@@ -8,31 +8,16 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func getDB(dsn string) (conn *pgx.Conn, err error) {
-	if conn, err = pgx.Connect(context.Background(), dsn); err != nil {
-		err = fmt.Errorf("db connect failed: %w", err)
-		return
-	}
-	return nil, nil
-}
-
-func freeDB(conn *pgx.Conn) (err error) {
-	if err = conn.Close(context.Background()); err != nil {
-		err = fmt.Errorf("db close failed: %w", err)
-	}
-	return
-}
-
 func Ping(dsn string) (err error) {
 	var (
 		conn *pgx.Conn
 	)
-	if conn, err = getDB(dsn); err != nil {
-		err = fmt.Errorf("get db failed: %w", err)
+	if conn, err = pgx.Connect(context.Background(), dsn); err != nil {
+		err = fmt.Errorf("db connect failed: %w", err)
 		return
 	}
 	defer func() {
-		if e := freeDB(conn); e != nil {
+		if e := conn.Close(context.Background()); e != nil {
 			err = errors.Join(err, fmt.Errorf("db close failed: %w", e))
 		}
 	}()
